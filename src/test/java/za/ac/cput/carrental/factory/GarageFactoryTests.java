7fe6ee9272ae0a9e2a0733da0381198952d698bd
solom <222359366@mycput.ac.za>
2026-06-28 20:03:12 +0200

    add Garage factory tests

--- src/test/java/za/ac/cput/carrental/factory/GarageFactoryTests.java
+++ src/test/java/za/ac/cput/carrental/factory/GarageFactoryTests.java
@@ -5,23 +5,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class GarageFactoryTests {
+
     @Test
-    void createGarage() {
-        Garage garage = GarageFactory.createGarage(
-                "G001",
-                "Cape Town Central Garage",
-                "101 Main Road",
-                "Cape Town",
-                "Sima Buthelezi"
-        );
+        void createGarageShouldNotBeNull() {
+            Garage garage = GarageFactory.createGarage(
+                    "G001",
+                    "Cape Town Central Garage",
+                    "101 Main Road",
+                    "Cape Town",
+                    "Sima Buthelezi"
+            );
 
-        assertNotNull(garage);
-        assertNotNull(garage.getGarageId());
-        assertEquals("Cape Town Central Garage", garage.getGarageName());
-        assertEquals("101 Main Road", garage.getAddress());
-        assertEquals("Cape Town", garage.getCity());
-        assertEquals("Sima Buthelezi", garage.getManagerName());
+            assertNotNull(garage);
+            System.out.println(garage);
+        }
 
-        System.out.println(garage);
+        @Test
+        void createGarageShouldSetGarageDetails() {
+            Garage garage = GarageFactory.createGarage(
+                    "G001",
+                    "Cape Town Central Garage",
+                    "101 Main Road",
+                    "Cape Town",
+                    "Sima Buthelezi"
+            );
+
+            assertEquals("Cape Town Central Garage", garage.getGarageName());
+            assertEquals("101 Main Road", garage.getAddress());
+            assertEquals("Cape Town", garage.getCity());
+            assertEquals("Sima Buthelezi", garage.getManagerName());
+        }
+
+        @Test
+        void createGarageShouldGenerateGarageId() {
+            Garage garage = GarageFactory.createGarage(
+                    "G001",
+                    "Cape Town Central Garage",
+                    "101 Main Road",
+                    "Cape Town",
+                    "Sima Buthelezi"
+            );
+
+            assertNotNull(garage.getGarageId());
+            assertFalse(garage.getGarageId().isEmpty());
+        }
     }
-}
