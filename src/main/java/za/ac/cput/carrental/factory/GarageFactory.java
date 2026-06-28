f1982b9070820068e4be080069a9f63204db53a0
solom <222359366@mycput.ac.za>
2026-06-28 16:46:07 +0200

    add Garage factory class

--- src/main/java/za/ac/cput/carrental/factory/GarageFactory.java
+++ src/main/java/za/ac/cput/carrental/factory/GarageFactory.java
@@ -1,4 +1,27 @@
 package za.ac.cput.carrental.factory;
+import za.ac.cput.carrental.util.Helper;
+import za.ac.cput.carrental.domain.Garage;
+
+/*
+ * GarageFactory.java
+ * Garage Factory class
+ * Author: 222359366
+ * Date: 28 June 2026
+ */
+
 
 public class GarageFactory {
+    public static Garage createGarage(String garageId,
+                                      String garageName,
+                                      String address,
+                                      String city,
+                                      String managerName) {
+        return new Garage.Builder()
+                .setGarageId(Helper.generateId())
+                .setGarageName(garageName)
+                .setAddress(address)
+                .setCity(city)
+                .setManagerName(managerName)
+                .build();
+    }
 }
