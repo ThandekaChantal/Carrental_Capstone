12a052382abd23c775934641619c539b3d0cd8ac
Tanatswa-Mabhodha-22 <220637482@mycput.ac.za>
2026-07-05 10:40:43 +0200

    Service & ServiceTest

--- src/main/java/za/ac/cput/carrental/service/BookingService.java
+++ src/main/java/za/ac/cput/carrental/service/BookingService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.carrental.domain.Booking;
 import za.ac.cput.carrental.factory.BookingFactory;
+import za.ac.cput.carrental.repository.BookingRepository;
+
 import java.util.List;
 
 @Service
