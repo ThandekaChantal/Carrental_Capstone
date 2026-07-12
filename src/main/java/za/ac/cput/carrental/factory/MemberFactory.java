782f46e5a70684919eff3d8882bd8331e3e74289
Stephanie Lewu <230211216@mycput.ac.za>
2026-07-12 22:14:08 +0200

    Changed variable type of phoneNumber to String. Completed the MemberFactory and MemberFactoryTests.

--- src/main/java/za/ac/cput/carrental/factory/MemberFactory.java
+++ src/main/java/za/ac/cput/carrental/factory/MemberFactory.java
@@ -8,12 +8,13 @@ import za.ac.cput.carrental.domain.Member;
 import za.ac.cput.carrental.util.Helper;
 
 public class MemberFactory {
-   public static Member createMember(String name,
+   public static Member createMember(String memberId,
+                                     String name,
                                      String licenseNumber,
                                      String phoneNumber,
                                      String accountStatus){
        return new Member.Builder()
-               .setMemberId(Helper.generateId())
+               .setMemberId(memberId)
                .setName(name)
                .setLicenseNumber(licenseNumber)
                .setPhoneNumber(phoneNumber)
--- src/main/java/za/ac/cput/carrental/service/MemberService.java
+++ src/main/java/za/ac/cput/carrental/service/MemberService.java
@@ -1,4 +1,62 @@
+/*MemberService.java
+Author: Stephanie Tola Oluwafemi Lewu
+Student Number: 230211216
+07 July 2026
+ */
+
 package za.ac.cput.carrental.service;
 
-public class MemberService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.carrental.domain.Member;
+import za.ac.cput.carrental.factory.MemberFactory;
+import za.ac.cput.carrental.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+public class MemberService implements IMemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public Member create(String memberId,
+                         String name,
+                         String licenseNumber,
+                         String phoneNumber,
+                         String accountStatus) {
+
+        Member member = MemberFactory.createMember(memberId, name, licenseNumber, phoneNumber, accountStatus);
+        return memberRepository.save(member);
+    }
+
+    @Override
+    public Member read(String memberId){return memberRepository.findById(memberId).orElse(null);}
+
+    @Override
+    public Member update(Member member){
+    return memberRepository.save(member);
+    }
+
+    @Override
+    public boolean delete(String memberId){
+        if (memberRepository.existsById(memberId)) {
+            memberRepository.deleteById(memberId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Member> getAll() {
+        return memberRepository.findAll();
+    }
 }
+
+
+
